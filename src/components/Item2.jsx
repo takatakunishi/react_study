fed2c0a44c0a274320c68891ca44e95015210ba0
KeitaOsaki <keita.base.05@gmail.com>
2019-09-23 17:13:34 +0900

    [fix]item2の背景色を変更

--- src/components/Item2.jsx
+++ src/components/Item2.jsx
@@ -2,6 +2,45 @@ import React from 'react';
 function Item2(){
   return(
     <div id="item2">item2</div>
+    <div id="container">
+  <h1>Different CSS Buttons hover effects </h1>
+  <div class="button-1">
+    <div class="eff-1"></div>
+    <a href="#"> Touch me </a>
+  </div>
+  <div class="button-4">
+    <div class="eff-4"></div>
+    <a href="#"> Touch me </a>
+  </div>
+  <div class="button-2">
+    <div class="eff-2"></div>
+    <a href="#"> Touch me </a>
+  </div>
+  <div class="button-3">
+    <div class="eff-3"></div>
+    <a href="#"> Touch me </a>
+  </div>
+  <div class="button-5">
+    <div class="eff-5"></div>
+    <a href="#"> Touch me </a>
+  </div>
+  <div class="button-6">
+    <div class="eff-6"></div>
+    <a href="#"> Touch me </a>
+  </div>
+  <div class="button-7">
+    <div class="eff-7"></div>
+    <a href="#"> Touch me </a>
+  </div>
+  <div class="button-8">
+    <div class="eff-8"></div>
+    <a href="#"> Touch me </a>
+  </div>
+</div>
+<footer>
+  <h1> Follow me for more awesome css pens <a href="https://codepen.io/Kiraken/" target="_blank"> here </a></h1>
+</footer>
+
   );
 }
 

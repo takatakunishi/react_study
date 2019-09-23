f7da5c2aaac9c4c1fa04993fd73562af4e7af61d
Takashi Nishikawa <program.takashi@gmail.com>
2019-09-23 17:13:38 +0900

    [fix] fix about nav contents

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 function Nav(){
   return(
-    <div id="nav">nav</div>
+    <div id="nav">
+      <div>
+        <p>contents</p>
+        <ul>
+          <li>item1</li>
+          <li>item2</li>
+        </ul>
+      </div>
+    </div>
   );
 }
 

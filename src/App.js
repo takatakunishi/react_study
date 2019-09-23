361e74123ae8b1a6af8642e626b86ed9bfc3264a
Takashi Nishikawa <program.takashi@gmail.com>
2019-09-23 13:40:50 +0900

    [start & fix] making with glid-layout
    グリッドの練習

--- src/App.js
+++ src/App.js
@@ -4,21 +4,12 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="container">
+      <div id="header">header</div>
+      <div id="nav">nav</div>
+      <div id="item-1">item-1</div>
+      <div id="item-2">item-2</div>
+
     </div>
   );
 }
